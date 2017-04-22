9d985ff172ce3bd192c4d39fb9e41f2c31fe4dda
TheMejmun <saman14m@gmail.com>
2017-04-22 19:16:28 +0200

    Field positions

--- src/game/Field.java
+++ src/game/Field.java
@@ -1,6 +1,7 @@
 package game;
 
 import java.util.ArrayList;
+import org.lwjgl.util.vector.Vector2f;
 
 import game.units.Unit;
 import lombok.Getter;
@@ -10,16 +11,36 @@ import lombok.Setter;
 
 @RequiredArgsConstructor
 public class Field {
+
+	private static final Vector2f DIMENSIONS = new Vector2f(1, (float) (2 / Math.sqrt(3)));
+
 	public ArrayList<Unit> units = new ArrayList<Unit>();
-	private @Getter final int posX, posY;
+	private @Getter final int rawPosX, rawPosY;
 	private @NonNull @Getter @Setter FieldType type;
 	private @Getter Field[] neighbors = new Field[6];
 
 	void setneighbors(Field[][] fields) {
-		
+
+		int rightShift = isLefter() ? 0 : 1;
+
+		neighbors[0] = fields[rawPosX + rightShift][rawPosY - 1];
+		neighbors[1] = fields[rawPosX + 1][rawPosY];
+		neighbors[2] = fields[rawPosX + rightShift][rawPosY + 1];
+		neighbors[3] = fields[rawPosX - 1 + rightShift][rawPosY + 1];
+		neighbors[4] = fields[rawPosX - 1][rawPosY];
+		neighbors[5] = fields[rawPosX - 1 + rightShift][rawPosY - 1];
+
+	}
+
+	public float getX() {
+		return rawPosX + (isLefter() ? 0 : 0.5f);
+	}
+
+	public float getY() {
+		return rawPosY * DIMENSIONS.y * 0.75f;
 	}
 
 	public boolean isLefter() {
-		return posY % 2 == 0;
+		return rawPosY % 2 == 0;
 	}
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -5,15 +5,16 @@ public class Game {
 	public final static int FIELDS_X = 32;
 	public final static int FIELDS_Y = 18;
 
-	static Field[][] fields = new Field[FIELDS_X][FIELDS_Y];
+	public static Field[][] fields = new Field[FIELDS_X][FIELDS_Y];
 
 	public static void init() {
 		// init Fields
-		for (int x = 0; x < FIELDS_X; x++)
+		for (int x = 0; x < FIELDS_X; x++) {
 			for (int y = 0; y < FIELDS_Y; y++) {
 				fields[x][y] = new Field(x, y, FieldType.GRASS);
+				fields[x][y].setneighbors(fields);
 			}
-		
-		
+		}
+
 	}
 }
